db4a2ca0adc43b1344f92cd7bfde0db608fba207
David Nelson <nelsondavidmark@gmail.com>
2019-09-20 12:32:27 +0700

    Initial commit with Bulma and Font Awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'share-family-recipes';
+  faCoffee = faCoffee;
 }
