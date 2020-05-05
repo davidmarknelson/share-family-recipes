a70eca299627246fd5372ea5d438e3c97871498a
David Nelson <nelsondavidmark@gmail.com>
2020-05-05 08:17:45 -0400

    Temp async commit

--- src/app/pages/recipes/recipe-view/recipe-view.component.spec.ts
+++ src/app/pages/recipes/recipe-view/recipe-view.component.spec.ts
@@ -1,40 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { DebugElement } from "@angular/core";
 import { Router } from "@angular/router";
-import { RecipeViewComponent } from './recipe-view.component';
-import { RecipesModule } from '../recipes.module';
-import { ActivatedRoute} from '@angular/router';
-import { RecipeService } from '../../../utilities/services/recipe/recipe.service';
-import { AuthService } from '../../../utilities/services/auth/auth.service';
-import { of, throwError, Observable } from 'rxjs';
-import { By } from '@angular/platform-browser';
-import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RecipeViewComponent } from "./recipe-view.component";
+import { RecipesModule } from "../recipes.module";
+import { ActivatedRoute } from "@angular/router";
+import { RecipeService } from "@utilities/services/recipe/recipe.service";
+import { AuthService } from "@utilities/services/auth/auth.service";
+import { of, throwError, Observable } from "rxjs";
+import { By } from "@angular/platform-browser";
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 const recipeObj = {
   id: 1,
   name: "Eggs and Rice",
   description: "A delicious and easy dish.",
   creatorId: 1,
-  creator: { 
-    username: "johndoe", 
+  creator: {
+    username: "johndoe",
     profilePic: {
-      profilePicName: "../../../assets/images/default-img/default-profile-pic.jpg"
-    } 
+      profilePicName:
+        "../../../assets/images/default-img/default-profile-pic.jpg",
+    },
   },
-  ingredients: [ "3 eggs", "rice", "vegetables" ],
-  instructions: [ "cooks eggs", "cook rice", "mix and serve" ],
+  ingredients: ["3 eggs", "rice", "vegetables"],
+  instructions: ["cooks eggs", "cook rice", "mix and serve"],
   cookTime: 20,
   difficulty: 1,
   likes: [],
   savedRecipes: [],
-  mealPic: { 
-    mealPicName: "../../../assets/images/default-img/default-meal-pic.jpg" 
+  mealPic: {
+    mealPicName: "../../../assets/images/default-img/default-meal-pic.jpg",
   },
   createdAt: "Dec 04, 2019",
   updatedAt: "Dec 04, 2019",
-  originalRecipeUrl: 'http://www.originalrecipe.com',
-  youtubeUrl: "https://www.youtube-nocookie.com/embed/MV0F_XiR48Q"
+  originalRecipeUrl: "http://www.originalrecipe.com",
+  youtubeUrl: "https://www.youtube-nocookie.com/embed/MV0F_XiR48Q",
 };
 
 const recipe2Obj = {
@@ -42,22 +43,23 @@ const recipe2Obj = {
   name: "Eggs and Rice",
   description: "A delicious and easy dish.",
   creatorId: 1,
-  creator: { 
-    username: "johndoe#1", 
+  creator: {
+    username: "johndoe#1",
     profilePic: {
-      profilePicName: "../../../assets/images/default-img/default-profile-pic.jpg"
-    } 
+      profilePicName:
+        "../../../assets/images/default-img/default-profile-pic.jpg",
+    },
   },
-  ingredients: [ "3 eggs", "rice", "vegetables" ],
-  instructions: [ "cooks eggs", "cook rice", "mix and serve" ],
+  ingredients: ["3 eggs", "rice", "vegetables"],
+  instructions: ["cooks eggs", "cook rice", "mix and serve"],
   cookTime: 20,
   difficulty: 1,
   likes: [],
-  mealPic: { 
-    mealPicName: "../../../assets/images/default-img/default-meal-pic.jpg" 
+  mealPic: {
+    mealPicName: "../../../assets/images/default-img/default-meal-pic.jpg",
   },
   createdAt: "Dec 04, 2019",
-  updatedAt: "Dec 04, 2019"
+  updatedAt: "Dec 04, 2019",
 };
 
 const userObj = {
@@ -66,19 +68,19 @@ const userObj = {
   username: "johndoe",
   savedRecipes: [],
   iat: 1575496172,
-  exp: 2180296172
+  exp: 2180296172,
 };
 
-// This allows the recipe to be changed in the tests to 
+// This allows the recipe to be changed in the tests to
 // make it a number or a string.
 // It must be declared at the beginning of each test.
 let recipeItem;
 
 class MockActivatedRoute {
-  params = new Observable(observer => {
+  params = new Observable((observer) => {
     const recipe = {
-      recipe: recipeItem // declared above
-    }
+      recipe: recipeItem, // declared above
+    };
     observer.next(recipe);
     observer.complete();
   });
@@ -86,10 +88,10 @@ class MockActivatedRoute {
 
 class MockRecipeService {
   getRecipeByName(name) {
-    return of()
+    return of();
   }
   getRecipeById(id) {
-    return of()
+    return of();
   }
 }
 
@@ -98,187 +100,174 @@ class MockAuthService {
   currentUser() {}
 }
 
-class MockRouter {
-  navigate(path) {}
-  navigateByUrl(path) {}
-}
-
-describe('RecipeViewComponent', () => {
+fdescribe("RecipeViewComponent", () => {
   let component: RecipeViewComponent;
   let authService: AuthService;
   let activatedRoute: ActivatedRoute;
-  let router: Router;
   let recipeService: RecipeService;
   let fixture: ComponentFixture<RecipeViewComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RecipesModule, RouterTestingModule, HttpClientTestingModule]
+      imports: [RecipesModule, RouterTestingModule, HttpClientTestingModule],
     })
-    .overrideComponent(RecipeViewComponent, {
-      set: {
-        providers: [
-          { provide: RecipeService, useClass: MockRecipeService },
-          { provide: ActivatedRoute, useClass: MockActivatedRoute },
-          { provide: AuthService, useClass: MockAuthService }
-        ]
-      }
-    }).compileComponents();
+      .overrideComponent(RecipeViewComponent, {
+        set: {
+          providers: [
+            { provide: RecipeService, useClass: MockRecipeService },
+            { provide: ActivatedRoute, useClass: MockActivatedRoute },
+            { provide: AuthService, useClass: MockAuthService },
+          ],
+        },
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RecipeViewComponent);
     component = fixture.componentInstance;
-    router = fixture.debugElement.injector.get(Router);
     authService = fixture.debugElement.injector.get(AuthService);
     recipeService = fixture.debugElement.injector.get(RecipeService);
     activatedRoute = fixture.debugElement.injector.get(ActivatedRoute);
   });
 
-  it('should create', () => { 
-    recipeItem = '1';
+  it("should create", () => {
+    recipeItem = "1";
 
     fixture.detectChanges();
 
     expect(component).toBeTruthy();
   });
 
-  describe('error', () => {
-    it('should show an error message with the recipe id when the recipe does not exist', () => {
-      recipeItem = '1';
+  describe("error", () => {
+    it("should show an error message with the recipe id when the recipe does not exist", () => {
+      recipeItem = "1";
 
-      spyOn(component, 'getRecipeById').and.callThrough();
-      spyOn(recipeService, 'getRecipeById').and.callFake(() => {
+      spyOn(recipeService, "getRecipeById").and.callFake(() => {
         return throwError({
           error: {
-            message: 'That meal does not exist.'
-          }
+            message: "That meal does not exist.",
+          },
         });
       });
 
       fixture.detectChanges();
 
-      expect(component.getRecipeById).toHaveBeenCalled();
       expect(recipeService.getRecipeById).toHaveBeenCalled();
 
-      let errorMsg: DebugElement = fixture.debugElement.query(By.css('.message-body'));
-      
-      expect(errorMsg.nativeElement.innerText).toContain('That meal does not exist.')
+      let errorMsg: DebugElement = fixture.debugElement.query(
+        By.css(".message-body")
+      );
+
+      expect(errorMsg.nativeElement.innerText).toContain(
+        "That meal does not exist."
+      );
     });
 
-    it('should show an error message with the recipe name when the recipe does not exist', () => {
-      recipeItem = 'eggs';
+    it("should show an error message with the recipe name when the recipe does not exist", () => {
+      recipeItem = "eggs";
 
-      spyOn(component, 'getRecipeByName').and.callThrough();
-      spyOn(recipeService, 'getRecipeByName').and.callFake(() => {
+      spyOn(recipeService, "getRecipeByName").and.callFake(() => {
         return throwError({
           error: {
-            message: 'That meal does not exist.'
-          }
+            message: "That meal does not exist.",
+          },
         });
       });
 
       fixture.detectChanges();
 
-      expect(component.getRecipeByName).toHaveBeenCalled();
       expect(recipeService.getRecipeByName).toHaveBeenCalled();
 
-      let errorMsg: DebugElement = fixture.debugElement.query(By.css('.message-body'));
-      
-      expect(errorMsg.nativeElement.innerText).toContain('That meal does not exist.')
+      let errorMsg: DebugElement = fixture.debugElement.query(
+        By.css(".message-body")
+      );
+
+      expect(errorMsg.nativeElement.innerText).toContain(
+        "That meal does not exist."
+      );
     });
   });
 
-  describe('recipe', () => {
-    it('should populate the page with the information and show the edit button for the creator', () => {
-      recipeItem = '1';
+  describe("recipe", () => {
+    it("should populate the page with the information and show the edit button for the creator", () => {
+      recipeItem = "1";
 
-      spyOn(authService, 'isLoggedIn').and.callFake(() => true);
-      spyOn(authService, 'currentUser').and.callFake(() => userObj);
+      spyOn(authService, "isLoggedIn").and.callFake(() => true);
+      spyOn(authService, "currentUser").and.callFake(() => userObj);
 
-      spyOn(component, 'getRecipeById').and.callThrough();
-      spyOn(recipeService, 'getRecipeById').and.callFake(() => {
-        return of(recipeObj);        
+      spyOn(recipeService, "getRecipeById").and.callFake(() => {
+        return of({ ...recipeObj });
       });
 
       fixture.detectChanges();
 
-      expect(component.getRecipeById).toHaveBeenCalled();
       expect(recipeService.getRecipeById).toHaveBeenCalled();
 
-      let title: DebugElement = fixture.debugElement.query(By.css('.page-header__title'));
-      let editBtn: DebugElement = fixture.debugElement.query(By.css('[data-test=edit-btn]'));
-      let iframeVideo: DebugElement = fixture.debugElement.query(By.css('iframe'));
+      let title: DebugElement = fixture.debugElement.query(
+        By.css(".page-header__title")
+      );
+      let editBtn: DebugElement = fixture.debugElement.query(
+        By.css("[data-test=edit-btn]")
+      );
+      let iframeVideo: DebugElement = fixture.debugElement.query(
+        By.css("iframe")
+      );
 
-      expect(title.nativeElement.innerText).toEqual('Eggs and Rice');
+      expect(title.nativeElement.innerText).toEqual("Eggs and Rice");
       expect(editBtn).toBeTruthy();
       expect(iframeVideo).toBeTruthy();
     });
-    
-    it('should populate the page with the information and show the edit button for the creator and call getRecipeByName', () => {
-      recipeItem = 'eggs';
 
-      spyOn(authService, 'isLoggedIn').and.callFake(() => true);
-      spyOn(authService, 'currentUser').and.callFake(() => userObj);
+    it("should populate the page with the information and show the edit button for the creator and call getRecipeByName", () => {
+      recipeItem = "eggs";
 
-      spyOn(component, 'getRecipeByName').and.callThrough();
-      spyOn(recipeService, 'getRecipeByName').and.callFake(() => {
-        return of(recipeObj);        
+      spyOn(authService, "isLoggedIn").and.callFake(() => true);
+      spyOn(authService, "currentUser").and.callFake(() => userObj);
+
+      spyOn(recipeService, "getRecipeByName").and.callFake(() => {
+        return of({ ...recipeObj });
       });
 
       fixture.detectChanges();
 
-      expect(component.getRecipeByName).toHaveBeenCalled();
       expect(recipeService.getRecipeByName).toHaveBeenCalled();
 
-      let title: DebugElement = fixture.debugElement.query(By.css('.page-header__title'));
-      let editBtn: DebugElement = fixture.debugElement.query(By.css('[data-test=edit-btn]'));
-      let iframeVideo: DebugElement = fixture.debugElement.query(By.css('iframe'));
+      let title: DebugElement = fixture.debugElement.query(
+        By.css(".page-header__title")
+      );
+      let editBtn: DebugElement = fixture.debugElement.query(
+        By.css("[data-test=edit-btn]")
+      );
+      let iframeVideo: DebugElement = fixture.debugElement.query(
+        By.css("iframe")
+      );
 
-      expect(title.nativeElement.innerText).toEqual('Eggs and Rice');
+      expect(title.nativeElement.innerText).toEqual("Eggs and Rice");
       expect(editBtn).toBeTruthy();
       expect(iframeVideo).toBeTruthy();
     });
 
-    it('should populate the page with the information and not show the edit button', () => {
-      recipeItem = '1';
+    it("should populate the page with the information and not show the edit button", () => {
+      recipeItem = "1";
 
-      spyOn(component, 'getRecipeById').and.callThrough();
-      spyOn(recipeService, 'getRecipeById').and.callFake(() => {
-        return of(recipe2Obj);        
+      spyOn(recipeService, "getRecipeById").and.callFake(() => {
+        return of({ ...recipe2Obj });
       });
 
       fixture.detectChanges();
 
-      expect(component.getRecipeById).toHaveBeenCalled();
       expect(recipeService.getRecipeById).toHaveBeenCalled();
 
-      let title: DebugElement = fixture.debugElement.query(By.css('.page-header__title'));
-      let editBtn: DebugElement = fixture.debugElement.query(By.css('[data-test=edit-btn]'));
+      let title: DebugElement = fixture.debugElement.query(
+        By.css(".page-header__title")
+      );
+      let editBtn: DebugElement = fixture.debugElement.query(
+        By.css("[data-test=edit-btn]")
+      );
 
-      expect(title.nativeElement.innerText).toEqual('Eggs and Rice');
+      expect(title.nativeElement.innerText).toEqual("Eggs and Rice");
       expect(editBtn).toBeFalsy();
-    });
-
-    it('should call navigateByUrl with an encoded username when the creator username is clicked', () => {
-      recipeItem = '1';
-
-      spyOn(component, 'getRecipeById').and.callThrough();
-      spyOn(recipeService, 'getRecipeById').and.callFake(() => {
-        return of(recipe2Obj);        
-      });
-
-      fixture.detectChanges();
-
-      spyOn(router, "navigateByUrl");
-      spyOn(component, 'goToUsersRecipes').and.callThrough()
-
-      let usernameLink: DebugElement = fixture.debugElement.query(By.css('[data-test=username-link]'));
-      usernameLink.nativeElement.click()
-      fixture.detectChanges();
-
-      expect(component.goToUsersRecipes).toHaveBeenCalled();
-      expect(router.navigateByUrl).toHaveBeenCalledWith('/recipes/user-recipes?username=johndoe%231');
     });
   });
 });
--- src/app/pages/recipes/recipe-view/recipe-view.component.ts
+++ src/app/pages/recipes/recipe-view/recipe-view.component.ts
@@ -1,107 +1,46 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Observable, iif, throwError, of } from "rxjs";
+import {
+  tap,
+  map,
+  switchMap,
+  catchError,
+  distinctUntilChanged,
+} from "rxjs/operators";
 // Services
-import { RecipeService } from '../../../utilities/services/recipe/recipe.service';
-import { Recipe } from '../../../utilities/services/recipe/recipe';
-import { AuthService } from '../../../utilities/services/auth/auth.service';
-import { UserDecodedToken } from '../../../utilities/services/auth/user-decoded-token';
+import { RecipeService } from "@utilities/services/recipe/recipe.service";
+import { Recipe } from "@utilities/services/recipe/recipe";
+import { AuthService } from "@utilities/services/auth/auth.service";
+import { UserDecodedToken } from "@utilities/services/auth/user-decoded-token";
 // Font Awesome
-import { faFireAlt, faClock, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFireAlt,
+  faClock,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-recipe-view',
-  templateUrl: './recipe-view.component.html',
-  styleUrls: ['./recipe-view.component.scss']
+  selector: "app-recipe-view",
+  templateUrl: "./recipe-view.component.html",
+  styleUrls: ["./recipe-view.component.scss"],
 })
-export class RecipeViewComponent implements OnInit, OnDestroy {
-  private ngUnsubscribe = new Subject();
+export class RecipeViewComponent implements OnInit {
   user: UserDecodedToken;
   // Font Awesome
   faFireAlt = faFireAlt;
   faClock = faClock;
   faChevronRight = faChevronRight;
   // =======
-  recipeParam: string;
-  error: string;
-  recipe: Recipe;
-  loading: boolean = false;
-  isRecipeLiked: boolean;
-  isDataLoaded: boolean;
+  error: string = "";
 
   constructor(
-    private route: ActivatedRoute, 
-    private router: Router, 
+    private route: ActivatedRoute,
     private recipeService: RecipeService,
     private authService: AuthService
-  ) { }
+  ) {}
 
-  ngOnInit() {
-    this.route.params.pipe(
-      takeUntil(this.ngUnsubscribe)
-    ).subscribe(res => {
-      this.recipeParam = res['recipe'];
-
-      // if 'recipeParam' is a number(id), it will return the number
-      // and be a truthy value. If the value is a string(name), it will 
-      // return NaN and be a falsy value
-      if (Number(this.recipeParam)) {
-        this.getRecipeById(this.recipeParam);
-      } else {
-        this.getRecipeByName(this.recipeParam);
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    this.ngUnsubscribe.next();
-    this.ngUnsubscribe.complete();
-  }
-
-  getRecipeById(id) {
-    this.loading = true;
-
-    this.recipeService.getRecipeById(id).pipe(
-      takeUntil(this.ngUnsubscribe)
-    ).subscribe(res => {
-      this.loading = false;
-
-      // The user info must be called before
-      // the recipe is assigned because child components require the
-      // user data and the recipe data
-      this.returnUserObj();
-      this.error = '';
-      this.recipe = res;
-    }, err => {
-      this.loading = false;
-
-      // This shows the error message
-      this.error = err.error.message;
-    });
-  }
-
-  getRecipeByName(name) {
-    this.loading = true;
-
-    this.recipeService.getRecipeByName(name).pipe(
-      takeUntil(this.ngUnsubscribe)
-    ).subscribe(res => {
-      this.loading = false;
-
-      // The user info must be called before
-      // the recipe is assigned because child components require the
-      // user data and the recipe data
-      this.returnUserObj();
-      this.error = '';
-      this.recipe = res;
-    }, err => {
-      this.loading = false;
-
-      // This shows the error message
-      this.error = err.error.message;
-    });
-  }
+  ngOnInit() {}
 
   returnUserObj() {
     if (this.authService.isLoggedIn()) {
@@ -109,13 +48,23 @@ export class RecipeViewComponent implements OnInit, OnDestroy {
     }
   }
 
-  goToEditPage() {
-    this.router.navigateByUrl(`/create/edit?recipe=${this.recipe.id}`);
-  }
-
-  goToUsersRecipes(username) {
-    let encodedUsername = encodeURIComponent(username);
-    
-    this.router.navigateByUrl(`/recipes/user-recipes?username=${encodedUsername}`);
-  }
+  getRecipe$: Observable<Recipe> = this.route.params.pipe(
+    map((param) => param["recipe"]),
+    distinctUntilChanged(),
+    switchMap((param) =>
+      iif(
+        () => !!Number(param),
+        this.recipeService
+          .getRecipeById(param)
+          .pipe(tap(() => this.returnUserObj())),
+        this.recipeService
+          .getRecipeByName(param)
+          .pipe(tap(() => this.returnUserObj()))
+      )
+    ),
+    catchError((err) => {
+      this.error = err.error.message;
+      return of();
+    })
+  );
 }
